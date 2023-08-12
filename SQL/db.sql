46185738e94902f23e6839783fd9b2f1a99ea787
Ishan sharma <200518162@georgianc.on.ca>
2023-08-12 19:56:39 -0400

    changes

---
+++ SQL/db.sql
@@ -0,0 +1,10 @@
+-- Create the database
+CREATE DATABASE IF NOT EXISTS mydb_php;
+USE mydb_php;
+
+-- Create the users table
+CREATE TABLE IF NOT EXISTS users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL
+);
